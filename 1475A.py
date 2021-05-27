8dc0d3ef0204eb425011626c94d4112b8e1f0037
Tanjina Rahman <tanjina.3ni@gmail.com>
2021-05-27 15:53:02 +0600

    1475A

--- 1475A.py
+++ 1475A.py
@@ -5,3 +5,15 @@ Created on Wed May 26 23:07:56 2021
 @author: Aspire
 """
 
+t = int(input())
+
+for i in range(0,t):
+    n = int(input())
+    while n%2 == 0:
+        n = int(n/2)
+    if n==1:
+        print('NO')
+    else:
+        print('YES')
+    
+    
